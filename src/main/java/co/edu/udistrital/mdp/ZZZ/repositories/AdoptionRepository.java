8a6fcc3187a7e884f45da3c0c82435fc2c3eb451
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-09-06 22:32:57 -0500

    Delete ZZZ package that makes error and fix coexistenceTest entity

--- src/main/java/co/edu/udistrital/mdp/ZZZ/repositories/AdoptionRepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.udistrital.mdp.pets.repositories;
-
-import co.edu.udistrital.mdp.pets.entities.AdoptionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdoptionRepository extends JpaRepository<AdoptionEntity, Long> {
-}
